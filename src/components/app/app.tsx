c75b93e353fdb2f5601171d88bec68de44b017a7
4rutemu <artyom.denega@gmail.com>
2023-08-10 13:54:11 +0300

    private routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,11 @@
 import WelcomeScreen from '../../pages/welcome-page/welcome-page';
-import React from 'react';
+import React, {useState} from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import LoginPage from '../../pages/login-page/login-page';
-import FavoritesPage from "../../pages/favorites-page/favorites-page";
-import OfferPage from "../../pages/offer-page/offer-page";
-import PageNotFound from "../../pages/404-page/404-page";
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import PageNotFound from '../../pages/404-page/404-page';
+import PrivateRoute from "./private-route";
 
 type AppScreenProps = {
   favoritesCount: number;
@@ -15,13 +16,14 @@ function App({
   favoritesCount,
   placesFound
 }: AppScreenProps): JSX.Element {
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/'>
           <Route index element={<WelcomeScreen favoritesCount={favoritesCount} placesFound={placesFound}/>}></Route>
           <Route path='login' element={<LoginPage />}></Route>
-          <Route path='favorites' element={<FavoritesPage />}></Route>
+          <Route path='favorites' element={<PrivateRoute elements={<FavoritesPage />} />}></Route>
           <Route path='offer/'>
             <Route path=':id' element={<OfferPage />}></Route>
           </Route>
