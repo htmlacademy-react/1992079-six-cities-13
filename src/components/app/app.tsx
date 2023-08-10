e391f068a6cefc0ebf9741fba699a188d1b50d1f
4rutemu <artyom.denega@gmail.com>
2023-08-10 13:58:16 +0300

    linter fixes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,11 @@
 import WelcomeScreen from '../../pages/welcome-page/welcome-page';
-import React, {useState} from 'react';
+import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import LoginPage from '../../pages/login-page/login-page';
 import FavoritesPage from '../../pages/favorites-page/favorites-page';
 import OfferPage from '../../pages/offer-page/offer-page';
 import PageNotFound from '../../pages/404-page/404-page';
-import PrivateRoute from "./private-route";
+import PrivateRoute from './private-route';
 
 type AppScreenProps = {
   favoritesCount: number;
